620ec10c2f129c01e017630b0f9e22762485acd3
anna-wro <wro.online@gmail.com>
2017-02-04 11:24:02 +0100

    Initial commit

---
+++ app/src/main/java/pm/anna/takecare/CareActivity.java
@@ -0,0 +1,13 @@
+package pm.anna.takecare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_care);
+    }
+}
