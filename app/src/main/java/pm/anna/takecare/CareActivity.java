4e036ff37e672e8e9ff597bafcb19a49c79b0fe5
anna-wro <wro.online@gmail.com>
2017-02-04 14:10:08 +0100

    Disable button after click

--- app/src/main/java/pm/anna/takecare/CareActivity.java
+++ app/src/main/java/pm/anna/takecare/CareActivity.java
@@ -1,16 +1,15 @@
 package pm.anna.takecare;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class CareActivity extends BaseActivity {
+
+public class CareActivity extends BaseActivity  {
     private TextView mHowMany;
     private Button mButton;
     int howMany = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -18,20 +17,33 @@ public class CareActivity extends BaseActivity {
         setContentView(R.layout.activity_care);
     }
     public void addThreePoints(View v) {
+        Button b = (Button)v;
+        b.setTextColor(getResources().getColor(R.color.colorPrimary));
+        b.setBackgroundResource(R.drawable.button_used);
+        b.setEnabled(false);
         howMany +=3;
         addCare(howMany);
     }
     public void addTwoPoints(View v) {
+        Button b = (Button)v;
+        b.setTextColor(getResources().getColor(R.color.colorPrimary));
+        b.setBackgroundResource(R.drawable.button_used);
+        b.setEnabled(false);
         howMany +=2;
         addCare(howMany);
     }
     public void addOnePoint(View v) {
+        Button b = (Button)v;
+        b.setTextColor(getResources().getColor(R.color.colorPrimary));
+        b.setBackgroundResource(R.drawable.button_used);
+        b.setEnabled(false);
         howMany +=1;
         addCare(howMany);
     }
     public void resetCare (View v){
         howMany = 0;
         addCare(howMany);
+
     }
     public void addCare (int num) {
         mHowMany = (TextView) findViewById(R.id.howMany);
