61d9d4afbeb3733677023d49d29e28574c7bebf6
anna-wro <wro.online@gmail.com>
2017-02-04 12:59:10 +0100

    Add project files

---
+++ app/src/main/java/pm/anna/takecare/BaseActivity.java
@@ -0,0 +1,39 @@
+package pm.anna.takecare;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class BaseActivity extends Activity {
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransitionExit();
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransitionEnter();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransitionExit();
+    }
+    /**
+     * Overrides the pending Activity transition by performing the "Enter" animation.
+     */
+    protected void overridePendingTransitionEnter() {
+        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+    }
+
+    /**
+     * Overrides the pending Activity transition by performing the "Exit" animation.
+     */
+    protected void overridePendingTransitionExit() {
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+    }
+
+
+}
--- app/src/main/java/pm/anna/takecare/CareActivity.java
+++ app/src/main/java/pm/anna/takecare/CareActivity.java
@@ -2,12 +2,39 @@ package pm.anna.takecare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CareActivity extends AppCompatActivity {
+public class CareActivity extends BaseActivity {
+    private TextView mHowMany;
+    private Button mButton;
+    int howMany = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_care);
+    }
+    public void addThreePoints(View v) {
+        howMany +=3;
+        addCare(howMany);
+    }
+    public void addTwoPoints(View v) {
+        howMany +=2;
+        addCare(howMany);
+    }
+    public void addOnePoint(View v) {
+        howMany +=1;
+        addCare(howMany);
+    }
+    public void resetCare (View v){
+        howMany = 0;
+        addCare(howMany);
+    }
+    public void addCare (int num) {
+        mHowMany = (TextView) findViewById(R.id.howMany);
+        mHowMany.setText(String.valueOf(num));
     }
 }
