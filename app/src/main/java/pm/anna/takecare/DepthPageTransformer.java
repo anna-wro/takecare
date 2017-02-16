478ffcf3d4247ff4443c25b50af403fcd615ac90
anna-wro <wro.online@gmail.com>
2017-02-16 18:03:55 +0100

    :bookmark: Archive layout

--- app/src/main/java/pm/anna/takecare/DepthPageTransformer.java
+++ app/src/main/java/pm/anna/takecare/DepthPageTransformer.java
@@ -3,7 +3,11 @@ package pm.anna.takecare;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+/**
+ * Created by Anna on 15.02.2017.
+ */
 public class DepthPageTransformer implements ViewPager.PageTransformer {
+
     public void transformPage(View view, float position) {
         int pageWidth = view.getWidth();
 
@@ -14,6 +18,7 @@ public class DepthPageTransformer implements ViewPager.PageTransformer {
         } else if (position <= 1) { // (0,1]
             // Counteract the default slide transition
             view.setTranslationX(pageWidth * -position);
+
         }
     }
 }
