dba6d12606e436f42219e4a12d4e213fba093f63
anna-wro <wro.online@gmail.com>
2017-02-18 19:32:01 +0100

    :package: Refactoring

--- app/src/main/java/pm/anna/takecare/CareActivity.java
+++ app/src/main/java/pm/anna/takecare/CareActivity.java
@@ -27,6 +27,9 @@ import static pm.anna.takecare.R.id.addButton;
 
 public class CareActivity extends BaseActivity {
     int howMany = 0;
+    int bodyPoints = 0;
+    int mindPoints = 0;
+    int soulPoints = 0;
     String howManyBody = "";
     String howManyMind = "";
     String howManySoul = "";
@@ -54,6 +57,7 @@ public class CareActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_care);
         initUiElements();
+
         WizardPagerAdapter adapter = new WizardPagerAdapter();
         mViewPager.setAdapter(adapter);
         mViewPager.setOffscreenPageLimit(5);
@@ -110,27 +114,15 @@ public class CareActivity extends BaseActivity {
     public void insertArchiveItem(View v) {
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
         String date = mDateEdit.getText().toString();
-        int allPoints = Integer.parseInt(mHowMany.getText().toString());
-        int bodyPoints = mBodyDots.length()/2;
-        int mindPoints = mMindDots.length()/2;
-        int soulPoints = mSoulDots.length()/2;
-        // Create a ContentValues object where column names are the keys,
-        // and Toto's pet attributes are the values.
         ContentValues values = new ContentValues();
         values.put(ArchiveEntry.COLUMN_DATE, date);
-        values.put(ArchiveEntry.COLUMN_POINTS_ALL, allPoints);
+        values.put(ArchiveEntry.COLUMN_POINTS_ALL, howMany);
         values.put(ArchiveEntry.COLUMN_POINTS_BODY, bodyPoints);
         values.put(ArchiveEntry.COLUMN_POINTS_MIND, mindPoints);
         values.put(ArchiveEntry.COLUMN_POINTS_SOUL, soulPoints);
 
-        // Insert a new row for Toto in the database, returning the ID of that new row.
-        // The first argument for db.insert() is the pets table name.
-        // The second argument provides the name of a column in which the framework
-        // can insert NULL in the event that the ContentValues is empty (if
-        // this is set to "null", then the framework will not insert a row when
-        // there are no values).
-        // The third argument is the ContentValues object containing the info for Toto.
         long newRowId = db.insert(ArchiveEntry.TABLE_NAME, null, values);
+
         displayDatabaseInfo();
         Context context = getApplicationContext();
         CharSequence text = "You've tracked another day! Yay!";
@@ -146,9 +138,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 5;
+            bodyPoints += 5;
             howManyBody += "● ● ● ● ● ";
         } else {
             howMany -= 5;
+            bodyPoints -= 5;
             howManyBody = howManyBody.substring(0, howManyBody.length() - 10);
         }
         changeCareBody(howManyBody);
@@ -159,9 +153,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 3;
+            bodyPoints += 3;
             howManyBody += "● ● ● ";
         } else {
             howMany -= 3;
+            bodyPoints -= 3;
             howManyBody = howManyBody.substring(0, howManyBody.length() - 6);
         }
         changeCareBody(howManyBody);
@@ -172,9 +168,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 2;
+            bodyPoints += 2;
             howManyBody += "● ● ";
         } else {
             howMany -= 2;
+            bodyPoints -= 2;
             howManyBody = howManyBody.substring(0, howManyBody.length() - 4);
         }
         changeCareBody(howManyBody);
@@ -185,9 +183,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 1;
+            bodyPoints += 1;
             howManyBody += "● ";
         } else {
             howMany -= 1;
+            bodyPoints -= 1;
             howManyBody = howManyBody.substring(0, howManyBody.length() - 2);
         }
         changeCareBody(howManyBody);
@@ -210,9 +210,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 5;
+            mindPoints += 5;
             howManyMind += "● ● ● ● ● ";
         } else {
             howMany -= 5;
+            mindPoints -= 5;
             howManyMind = howManyMind.substring(0, howManyMind.length() - 10);
         }
         changeCareMind(howManyMind);
@@ -223,9 +225,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 3;
+            mindPoints += 3;
             howManyMind += "● ● ● ";
         } else {
             howMany -= 3;
+            mindPoints -= 3;
             howManyMind = howManyMind.substring(0, howManyMind.length() - 6);
         }
         changeCareMind(howManyMind);
@@ -236,9 +240,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 2;
+            mindPoints += 2;
             howManyMind += "● ● ";
         } else {
             howMany -= 2;
+            mindPoints -= 2;
             howManyMind = howManyMind.substring(0, howManyMind.length() - 4);
         }
         changeCareMind(howManyMind);
@@ -249,9 +255,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 1;
+            mindPoints += 1;
             howManyMind += "● ";
         } else {
             howMany -= 1;
+            mindPoints -= 1;
             howManyMind = howManyMind.substring(0, howManyMind.length() - 2);
         }
         changeCareMind(howManyMind);
@@ -273,9 +281,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 5;
+            soulPoints += 5;
             howManySoul += "● ● ● ● ● ";
         } else {
             howMany -= 5;
+            soulPoints -= 5;
             howManySoul = howManySoul.substring(0, howManySoul.length() - 10);
         }
         changeCareSoul(howManySoul);
@@ -286,9 +296,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 3;
+            soulPoints += 3;
             howManySoul += "● ● ● ";
         } else {
             howMany -= 3;
+            soulPoints -= 3;
             howManySoul = howManySoul.substring(0, howManySoul.length() - 6);
         }
         changeCareSoul(howManySoul);
@@ -299,9 +311,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 2;
+            soulPoints += 2;
             howManySoul += "● ● ";
         } else {
             howMany -= 2;
+            soulPoints -= 2;
             howManySoul = howManySoul.substring(0, howManySoul.length() - 4);
         }
         changeCareSoul(howManySoul);
@@ -312,9 +326,11 @@ public class CareActivity extends BaseActivity {
         boolean isChecked = ((ToggleButton) v).isChecked();
         if (isChecked) {
             howMany += 1;
+            soulPoints += 1;
             howManySoul += "● ";
         } else {
             howMany -= 1;
+            soulPoints -= 1;
             howManySoul = howManySoul.substring(0, howManySoul.length() - 2);
         }
         changeCareSoul(howManySoul);
